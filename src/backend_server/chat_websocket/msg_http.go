8d2981cec9820c31ab4053e96e0de18b692130d5
Junqi Ye <jy98@uw.edu>
2023-07-31 21:23:12 -0700

    added post request for new conversation

--- src/backend_server/chat_websocket/msg_http.go
+++ src/backend_server/chat_websocket/msg_http.go
@@ -55,19 +55,60 @@ func marshalHistDataToSend(msgs []msgObj) []byte {
 
 func httpConvIDAPIEndpoint(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	log.Println("new convid request:", r.URL)
+	q := r.URL.Query()
+	w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
 
 	switch r.Method {
 	case "GET":
-		q := r.URL.Query()
 		userID := q.Get("userID")
 		log.Println("query", userID)
-		if userID != "" {
-			ids := hub.storage.getAllConvIDsFromUserID(userID)
-			log.Println("ids :", ids)
-			w.Header().Add("Content-Type", "application/json")
-			w.Header().Add("Access-Control-Allow-Origin", "*")
-			w.Write(marshalConvIDDataToSend(userID, ids))
+		if userID == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			break
 		}
+
+		ids := hub.storage.getAllConvIDsFromUserID(userID)
+		log.Println("ids :", ids)
+		w.Write(marshalConvIDDataToSend(userID, ids))
+		break
+
+	case "POST":
+		senderID := q.Get("senderID")
+		recipientID := q.Get("recipientID")
+
+		if senderID == "" || recipientID == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			break
+		}
+
+		conversationID, exists := hub.checkConvIDExist(senderID, recipientID)
+
+		if !exists {
+			log.Println("new conversation created", conversationID)
+			// init server side conversation
+			hub.conversations[conversationID][senderID] = true
+			hub.conversations[conversationID][recipientID] = true
+			hub.conversation_msg_counter[conversationID] = 0
+
+			hub.storage.storeConvID(conversationID, senderID, recipientID)
+		}
+
+		type temp struct {
+			ConvID string `json:"convID"`
+		}
+		temp2 := temp{}
+		temp2.ConvID = conversationID
+		log.Println(temp2)
+		b, err := json.Marshal(temp2)
+		if err != nil {
+			log.Println(err)
+		}
+		w.Write(b)
+
+		// w.Header().Add()
+		// w.WriteHeader(http.StatusCreated)
+
 		break
 	default:
 		w.WriteHeader(http.StatusBadRequest)
