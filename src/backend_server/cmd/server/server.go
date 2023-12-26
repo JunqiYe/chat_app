0d06894c92ce277898fb838a1c2c103eb2febdce
Junqi Ye <jy98@uw.edu>
2023-12-26 12:02:07 -0800

    adding flag to check with which table to use

--- src/backend_server/cmd/server/server.go
+++ src/backend_server/cmd/server/server.go
@@ -4,7 +4,9 @@ import (
 	"backend_server/internal/apiEndpoint"
 	"backend_server/internal/store/cloudStore"
 	"context"
+	"flag"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
@@ -28,9 +30,33 @@ func main() {
 
 	// using local store implemented with sqlite
 	// sqlLiteStore := localStore.NewStorage(filepath.Join(currDir, dbDir))
+	var historyTableName string
+	var convMemberTableName string
 
+	// output log to separate file
+	f, err := os.OpenFile("log/testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
+	// check flag for which table to use
+	testFlagPtr := flag.Bool("test", false, "using the testing table or production table")
+	flag.Parse()
+	if !(*testFlagPtr) {
+		historyTableName = "MessageHistory"
+		convMemberTableName = "ConversationMembers"
+	} else {
+		historyTableName = "MessageHistory_test"
+		convMemberTableName = "ConversationMembers_test"
+
+	}
+
+	// loading aws credentials
 	log.Println("Loading AWS config...")
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion("us-west-1"))
 	if err != nil {
 		log.Fatalf("failed to load configuration, \n%v", err)
 	}
@@ -38,9 +64,19 @@ func main() {
 	client := dynamodb.NewFromConfig(cfg)
 	DynamoDBStore := &cloudStore.CloudStore{
 		DynamoDbClient:               client,
-		MessageHistoryTableName:      "MessageHistory",
-		ConversationMembersTableName: "ConversationMembers",
+		MessageHistoryTableName:      historyTableName,
+		ConversationMembersTableName: convMemberTableName,
 	}
+
+	// tests
+	tableList, err := DynamoDBStore.ListTables()
+	if err != nil {
+		log.Fatalf("[TEST]: error:%s", err.Error())
+	}
+	for _, table := range tableList {
+		log.Printf("[TEST]:%s\n", table)
+	}
+
 	hub := apiEndpoint.NewHub(DynamoDBStore)
 
 	go hub.HubRun()
